98b8d8e147583c97d9dd8968279b6833d7c5ada8
sachsnathanw28 <nathan.wu27@ycdsbk12.ca>
2026-06-03 14:04:51 -0400

    W.I.P clickable grid 2

--- src/Sketch.java
+++ src/Sketch.java
@@ -5,6 +5,11 @@ import processing.core.PApplet;
  * @author Your Name
  */
 public class Sketch extends PApplet {
+
+    int columns = 15;
+    int rows = 8;
+    boolean[][] grid = new boolean[columns][rows]
+
     public static void main(String[] args) {
         PApplet.main("Sketch");
     }
@@ -16,7 +21,43 @@ public class Sketch extends PApplet {
 
     @Override
     public void setup() {
+        
+        for (int c = 0; c < columns; c++) {
+            for (int r = 0; r < rows; r++) {
+                grid[c][r] = false;
+                }
+            }
+        }
 
+    
+    }
+    noStroke();
+    fill(110, 115, 100);
+    rect(0,300,1000,60);
+
+    // int[] yArray = {100, 160, 220, 280, 340, 460, 520};
+    String[] sounds = {"hi-hat.wav", "snare.wav", "kick.wav", "bass.wav", "synthPlusOne.wav", "synth.wav", "synthMinusOne.wav"};
+
+    boolean[][] grid = new boolean[3][3];
+
+    for (int i = 0; i < grid.length; i++) {
+        if(grid[i]) {
+            fill(0,255,0);
+        } else {
+            fill(255,255,255);
+        }
+
+    public void mousePressed(MouseEvent e) {
+           playAudio("kick.wav");
+           if (mousePressed) {
+           playAudio("kick.wav");
+           } 
+        }
+        
+    
+
+    @Override
+    public void draw() {
         background(110, 115, 100);
         fill(62, 64, 58);
         rect(0,0,1000,50);
@@ -35,40 +76,41 @@ public class Sketch extends PApplet {
         text("Synth",10, 460);
         text("Synth -1",10, 520);
 
-    for(int y = 25; y <= height - 120; y += 60){
+        stroke(0);
 
-        for(int i = 0; i < width; i += 60){
-            square(100 + i,y + 40,50);
-        }
-    }
-    noStroke();
-    fill(110, 115, 100);
-    rect(0,300,1000,60);
-
-    // int[] yArray = {100, 160, 220, 280, 340, 460, 520};
-    // String[] sounds = {"hi-hat", "snare", "kick", "bass", "synthPlusOne", "synth", "synthMinusOne"};
-
-    boolean[][] grid = new boolean[3][3];
-
-    for (int i = 0; i < grid.length; i++) {
-        if(grid[i] == true) {
-            fill(0,255,0);
+    for (int c = 0; c < columns; c++) {
+      for (int r = 0; r < rows; r++) {
+       
+        // Color choice based on grid state
+        if (grid[c][r]) {
+          fill(255, 0, 0); // Red = active
         } else {
-            fill(255,255,255);
+          fill(255);       // White = inactive
         }
+       
+        // Positions match your original design layout
+        square(100 + (c * 60), 65 + (r * 60), 50);
+      }
     }
+  }
 
-    }
-
-    @Override
-    public void draw() {
-        
     }
 
     public void mousePressed(){
         System.out.println("mouse clicked at: " + mouseX + ", " + mouseY);
+
+        if (mouseY < 500) {
+            playAudio("snare.wav");
+        } else if (mouseY > 501) {
+            playAudio("kick.wav");
+        }
     }
 
     /** Additional helper methods below */
+    private void playAudio(String fileName) {
 
-}
+        System.out.println("Now playing: " + fileName);
+
+    }
+
+
